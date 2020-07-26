d6d85969edd9d62b50be1a7f71d6beb6dcd81ba0
Nick <ptnguyen1901@gmail.com>
2020-07-25 23:29:15 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Nick Nguyen on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
