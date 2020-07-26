2fa5205e803b94c9283f61d968a8e1bf86b4d8e6
Nick <ptnguyen1901@gmail.com>
2020-07-25 23:42:31 -0400

    add menu

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -6,15 +6,26 @@
 //
 
 import SwiftUI
-
+// OPT + CMD + P : Resume
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!").padding()
+  
+  let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(menu) { section in 
+          Text(section.name)
+        }
+      }
+      .navigationTitle("Menu")
     }
+    
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
