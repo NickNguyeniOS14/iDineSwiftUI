c6b3925c99e569f5d638590ec6375c3498bdee50
Nick <ptnguyen1901@gmail.com>
2020-07-26 00:04:58 -0400

    Modify label

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ItemRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var item: MenuItem
+  static let colors: [String:Color] = ["D":.purple,"G":.black,"N":.red,"S":.blue,"V":.green]
+  
+  var body: some View {
+    HStack {
+      Image(item.thumbnailImage)
+        .clipShape(Circle())
+        .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+      VStack(alignment:.leading) {
+        Text(item.name)
+          .font(.headline)
+        Text("$\(item.price)")
+      }
+      Spacer()
+      
+      ForEach(item.restrictions,id:\.self) { restriction in
+        Text(restriction)
+          .font(.caption)
+          .fontWeight(.black)
+          .padding(5)
+          .background(Self.colors[restriction,default:.black])
+          .clipShape(Circle())
+          .foregroundColor(.white)
+      }
     }
+  }
 }
 
 struct ItemRow_Previews: PreviewProvider {
-    static var previews: some View {
-        ItemRow()
-    }
+  static var previews: some View {
+    ItemRow(item: MenuItem.example)
+  }
 }
