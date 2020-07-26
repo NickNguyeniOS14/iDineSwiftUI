3bae68a6e84e2bfe6e4158aa9e2ef4e4f7ef4c92
Nick <ptnguyen1901@gmail.com>
2020-07-25 23:46:46 -0400

    change list style

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -15,10 +15,15 @@ struct ContentView: View {
     NavigationView {
       List {
         ForEach(menu) { section in 
-          Text(section.name)
+          Section(header: Text(section.name)) {
+            ForEach(section.items) { item in
+              Text(item.name)
+            }
+          }
         }
       }
       .navigationTitle("Menu")
+      .listStyle(GroupedListStyle())
     }
     
   }
